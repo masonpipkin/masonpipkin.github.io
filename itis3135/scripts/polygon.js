52cb441e9d07d80d6bcc09fcefadb7f719670bc5
masonpipkin <91095987+masonpipkin@users.noreply.github.com>
2022-02-21 21:40:52 -0500

    Polygon JavaScript page added.

---
+++ itis3135/scripts/polygon.js
@@ -0,0 +1,52 @@
+var number;
+do{
+    number = prompt('Please enter a number (1-10)');
+}
+while(!validateEntry(number))
+getShape(number);
+
+function getShape(input){
+    console.log(input)
+    switch(input){
+        case '0':
+            alert('notagon');
+            break
+        case '1':
+            alert('henagon');
+            break;
+        case '2': 
+            alert('digon');
+            break;
+        case '3':
+            alert('trigon');
+            break;
+        case '4':
+            alert('tetragon');
+            break;
+        case '5': 
+            alert('pentagon');
+            break;
+        case '6':
+            alert('hexagon');
+            break;
+        case '7':
+            alert('heptagon');
+            break;
+        case '8':
+            alert('octagon');
+            break;
+        case '9':
+            alert('enneagon');
+            break;
+        case '10':
+            alert('decagon');
+            break;
+    }
+}
+
+function validateEntry(entry){
+    if(entry >= 0 && entry <=10){
+        return true;
+    }
+    return false;   
+}
