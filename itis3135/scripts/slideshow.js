552fb2d99ad312db7b88b59ed3706127aae6b755
Mason Pipkin <91095987+masonpipkin@users.noreply.github.com>
2022-03-29 19:23:05 -0400

    slideshow "done"
    lol "done"

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
     $('.next').on('click', function(){
-      var currentImg = $('.active');
+      var currentImg = $('.current');
       var nextImg = currentImg.next();
   
       if(nextImg.length){
-        currentImg.removeClass('active').css('z-index', -10);
-        nextImg.addClass('active').css('z-index', 10);
+        currentImg.removeClass('current').css('z-index', -10);
+        nextImg.addClass('current').css('z-index', 10);
       }
     });
   
     $('.prev').on('click', function(){
-      var currentImg = $('.active');
+      var currentImg = $('.current');
       var prevImg = currentImg.prev();
   
       if(prevImg.length){
-        currentImg.removeClass('active').css('z-index', -10);
-        prevImg.addClass('active').css('z-index', 10);
+        currentImg.removeClass('current').css('z-index', -10);
+        prevImg.addClass('current').css('z-index', 10);
       }
     });
   });
