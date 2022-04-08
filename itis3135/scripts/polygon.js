43b41f896d03e6099af2c65ffb650fa9c1eb34f8
Mason Pipkin <91095987+masonpipkin@users.noreply.github.com>
2022-04-07 20:57:20 -0400

    Update polygon.js
    Fixed to round and include negative numbers

--- itis3135/scripts/polygon.js
+++ itis3135/scripts/polygon.js
@@ -1,52 +1,54 @@
 var number;
-do{
+do {
     number = prompt('Please enter a number (1-10)');
+    number = Math.round(number);
+    number = Math.abs(number);
 }
-while(!validateEntry(number))
+while (!validateEntry(number))
 getShape(number);
 
-function getShape(input){
+function getShape(input) {
     console.log(input)
-    switch(input){
-        case '0':
+    switch (input) {
+        case 0:
             alert('notagon');
             break
-        case '1':
+        case 1:
             alert('henagon');
             break;
-        case '2': 
+        case 2:
             alert('digon');
             break;
-        case '3':
+        case 3:
             alert('trigon');
             break;
-        case '4':
+        case 4:
             alert('tetragon');
             break;
-        case '5': 
+        case 5:
             alert('pentagon');
             break;
-        case '6':
+        case 6:
             alert('hexagon');
             break;
-        case '7':
+        case 7:
             alert('heptagon');
             break;
-        case '8':
+        case 8:
             alert('octagon');
             break;
-        case '9':
+        case 9:
             alert('enneagon');
             break;
-        case '10':
+        case 10:
             alert('decagon');
             break;
     }
 }
 
-function validateEntry(entry){
-    if(entry >= 0 && entry <=10){
+function validateEntry(entry) {
+    if (entry >= 0 && entry <= 10) {
         return true;
     }
-    return false;   
+    return false;
 }
