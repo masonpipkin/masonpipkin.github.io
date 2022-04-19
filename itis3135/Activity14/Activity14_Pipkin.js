825ed372f805c045129462b3acdef20b8263ae7c
Mason Pipkin <91095987+masonpipkin@users.noreply.github.com>
2022-04-18 23:19:12 -0400

    Activity13/14 Complete

---
+++ itis3135/Activity14/Activity14_Pipkin.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#nav_list li").click(function() {
+        let title = $(this).children("a").attr("title");
+        $.get(title + ".json", function(data, status) {
+            data = data['speakers'][0];
+            $("main h1").html(data['title']);
+            $("main h2").html(data['month']);
+            $("main h3").html(data['speaker']);
+            $("main img").attr("src", data.image);
+            $("main p").html(data.text);
+        });
+    });
+}); // end ready
